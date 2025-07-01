68a49d8773c9f86826ab077ea08721ce49655164
AlienInvasion <vijayvsph@gmail.com>
2025-07-01 14:07:06 +0530

    created settings.py

--- alieninvasion.py
+++ alieninvasion.py
@@ -1,10 +1,15 @@
 import sys, pygame
+from settings import Settings
 
 class AlienInvasion:
 
     def __init__(self):
+
+        '''Initialising Settings'''
+        self.settings = Settings()
+
         '''Initialising the screen attributes for the objects'''
-        self.screen = pygame.display.set_mode((1280, 720))
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption('Alien Invasion')
 
         '''Initialising the clock to lock frame rate'''
@@ -17,6 +22,8 @@ class AlienInvasion:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+            '''Fills the screen with given colour'''
+            self.screen.fill(self.settings.bg_colour)
             pygame.display.flip()
             '''Capping to 60 fps'''
             self.clock.tick(60)
---
+++ settings.py
@@ -0,0 +1,9 @@
+import pygame
+
+class Settings:
+    
+    def __init__(self):
+        self.screen_width = 1280
+        self.screen_height = 720
+        self.bg_colour = (230, 230, 230)
+        
